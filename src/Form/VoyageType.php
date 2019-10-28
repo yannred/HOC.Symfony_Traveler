d8b7ece3129967bf07dd3770bbf483801c4412a2
Lucas Delobelle <lucas@ld-web.net>
2019-10-28 13:38:26 +0100

    CRUD Destination, User, Voyage, création espace /admin

---
+++ src/Form/VoyageType.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Voyage;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class VoyageType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('titre')
+            ->add('description')
+            ->add('note')
+            ->add('dateDepart')
+            ->add('created')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Voyage::class,
+        ]);
+    }
+}
