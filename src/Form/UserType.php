61d7702bc55d1bbe484f5cf986cddf7be92faa4c
Lucas Delobelle <lucas@ld-web.net>
2019-10-30 11:18:17 +0100

    CRUD Utilisateur, Edit à faire

--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -4,6 +4,9 @@ namespace App\Form;
 
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,11 +15,17 @@ class UserType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('login')
-            ->add('roles')
-            ->add('password')
-            ->add('created')
-        ;
+            ->add('login', TextType::class, [
+                'label' => "Nom d'utilisateur"
+            ])
+            ->add('password', RepeatedType::class, [
+                'type' => PasswordType::class,
+                'invalid_message' => 'Les mots de passe doivent être identiques',
+                'options' => ['attr' => ['class' => 'password-field']],
+                'required' => true,
+                'first_options'  => ['label' => 'Mot de passe'],
+                'second_options' => ['label' => 'Confirmez le mot de passe']
+            ]);
     }
 
     public function configureOptions(OptionsResolver $resolver)
